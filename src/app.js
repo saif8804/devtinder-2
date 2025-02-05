7fa821c9dfab9d1d8e3dc1a6bd6b3cea13738acf
saif Ali <113436394+saif8804@users.noreply.github.com>
2025-02-05 12:18:55 +0530

    creating a server on express

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/test", (req, res) =>{
+      res.send(" How old are you")
+})
+
+
+
+app.listen(3000, () =>{
+     console.log("server is running on port 3000");
+})
